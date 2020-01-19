5220ac99e727ae212e13639f138dd7221ece4d91
starkend <danstarken@gmail.com>
2020-01-19 09:04:04 -0600

    Initial commit

---
+++ src/main/java/com/starkend/eskafka/EskafkaApplication.java
@@ -0,0 +1,13 @@
+package com.starkend.eskafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EskafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EskafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/starkend/eskafka/EskafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starkend.eskafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EskafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
